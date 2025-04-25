c0cd5176a453cabce3218c722f4c9b3f0543c06d
Swaroop <Swaroop1011@gmail.com>
2025-04-26 01:44:06 +0530

    commit 2 : adding new route and reading the second route params

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
 import { Text, View } from "react-native";
+import { Link } from 'expo-router';
+
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 justify-center items-center"
     >
-      <Text>Welcome!!!</Text>
+      <Text className="font-bold text-accent text-5xl">Welcome!!!</Text>
+      <Link href="/onboarding">Onboarding</Link>
+      <Link href="./movie/avengerssss">Avengers Link</Link>
+
     </View>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-const {getDefaultConig} = require("expo/metro-config")
+const {getDefaultConfig} = require("expo/metro-config")
 const {withNativeWind} = require("nativewind/metro")
 
-const config = getDefaultConig(__dirname)
+const config = getDefaultConfig(__dirname)
 
 module.exports = withNativeWind(config,
 
